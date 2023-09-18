8f7106ef5615d6293584f38e41f8d89a9f7e5580
Alvaro <alvarof260@gmail.com>
2023-09-18 15:19:29 -0300

    2do archivo

---
+++ code/ejercicio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int lado1,lado2,lado3,lado4;
+    printf("Ingrese el tamaño del lado 1 = ");
+    scanf("%d", &lado1);
+    printf("Ingrese el tamaño del lado 2 = ");
+    scanf("%d", &lado2);
+    printf("Ingrese el tamaño del lado 3 = ");
+    scanf("%d", &lado3);
+    printf("Ingrese el tamaño del lado 4 = ");
+    scanf("%d", &lado4);
+    return 0;
+}
